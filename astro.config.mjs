c0aecb25d208974b958be4878dbddba51b0fcb04
Cristian Felix Gonzalez Rojas <cristian.gonzalez.rojasgmail.com>
2026-03-23 13:45:30 -0300

    ci: configurar GitHub Actions para despliegue en GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://plai.cl',
+  site: 'https://CristianFGR.github.io',
+  base: '/sitio-web-plai',
   integrations: [tailwind(), sitemap()],
 });

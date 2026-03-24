27b80e95af4eb66aebea4b517039f69e62e05dab
Cristian Felix Gonzalez Rojas <cristian.gonzalez.rojasgmail.com>
2026-03-23 21:32:32 -0300

    ci: set conditional environments where main deploys to firebase and release branch to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+// Variables inyectadas por GitHub Actions al momento de compilar
+const isReleaseBranch = process.env.GITHUB_REF_NAME === 'release';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://CristianFGR.github.io',
-  base: '/sitio-web-plai',
+  site: isReleaseBranch ? 'https://CristianFGR.github.io' : 'https://plai.cl',
+  base: isReleaseBranch ? '/sitio-web-plai' : undefined,
   integrations: [tailwind()],
 });

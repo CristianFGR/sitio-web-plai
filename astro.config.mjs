87c1a7493b919c69c7b85c4e835d371f1b2e41b4
Cristian Felix Gonzalez Rojas <cristian.gonzalez.rojasgmail.com>
2026-03-26 22:46:32 -0300

    fix(astro): asegurar slash final en la variable base para cargar imágenes en Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 // Si corre en Github Actions, armamos para Github Pages. Si corre local o en otro CI, armamos para Firebase (root)
 export default defineConfig({
   site: isGithubActions ? 'https://CristianFGR.github.io' : 'https://sitio-web-plai.web.app',
-  base: isGithubActions ? '/sitio-web-plai' : undefined,
+  base: isGithubActions ? '/sitio-web-plai/' : '/',
   integrations: [tailwind(), sitemap()],
 });

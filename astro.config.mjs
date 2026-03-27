f08666c6adc538596d97c6cc7e8c03ca545094b6
Cristian Felix Gonzalez Rojas <cristian.gonzalez.rojasgmail.com>
2026-03-27 14:45:11 -0300

    fix: agregar foto webp de Cristian, asegurar variables de compilacion de Astro y resolver estilos en Firebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const isGithubPages = process.env.DEPLOY_TARGET === 'pages';
 
-// Si corre en Github Actions, armamos para Github Pages. Si corre local o en otro CI, armamos para Firebase (root)
+// Si corre en Github Actions (Pages), armamos con base url. Si corre local o en CI de Firebase, armamos para Firebase (root).
 export default defineConfig({
-  site: isGithubActions ? 'https://CristianFGR.github.io' : 'https://plai.cl',
-  base: isGithubActions ? '/sitio-web-plai/' : '/',
+  site: isGithubPages ? 'https://CristianFGR.github.io' : 'https://plai.cl',
+  base: isGithubPages ? '/sitio-web-plai/' : '/',
   integrations: [tailwind(), sitemap()],
 });

7bac2678f662574e9e51d34488ee9c31ae4b8450
Cristian Felix Gonzalez Rojas <cristian.gonzalez.rojasgmail.com>
2026-03-26 22:28:04 -0300

    fix(astro): resolución de conflictos manteniendo configuración base de Firebase y url dinámica

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+const isReleaseBranch = process.env.GITHUB_REF_NAME === 'release';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://CristianFGR.github.io',
-  base: '/sitio-web-plai',
-  integrations: [tailwind()],
+  site: isReleaseBranch ? 'https://sitio-web-plai.web.app' : 'https://plai.cl',
+  integrations: [tailwind(), sitemap()],
 });
